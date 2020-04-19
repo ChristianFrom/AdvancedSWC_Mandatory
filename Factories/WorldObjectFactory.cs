59b18a69c49192e48b7503498ff2b37acae886a3
Christian From Rasmussen <christianfrom@outlook.dk>
2020-04-19 14:20:48 +0200

    WorldObjectFactory tilføjet

---
+++ Factories/WorldObjectFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Advanced_Mandatory.Abstract_Classes;
+using Advanced_Mandatory.Enums;
+using Advanced_Mandatory.Interfaces;
+using Advanced_Mandatory.Models.GameObjects.WorldObjects;
+
+namespace Advanced_Mandatory.Factories
+{
+    public class WorldObjectFactory : IWorldObjectFactory
+    {
+        public WorldObjectBaseAbstract Create(WorldObjects worldObject)
+        {
+            if (worldObject == WorldObjects.HealthPotion) return new HealthPotion();
+            if (worldObject == WorldObjects.Spikes) return new Spikes();
+
+            throw new ArgumentException($"WorldObjectFactory - no object available with that this name {worldObject}");
+        }
+    }
+}
---
+++ Interfaces/IWorldObjectFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Advanced_Mandatory.Abstract_Classes;
+using Advanced_Mandatory.Enums;
+
+namespace Advanced_Mandatory.Interfaces
+{
+    public interface IWorldObjectFactory
+    {
+        WorldObjectBaseAbstract Create(WorldObjects worldObject);
+    }
+}
